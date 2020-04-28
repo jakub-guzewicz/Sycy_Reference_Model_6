952e8440b338c5450194444da8e3c86ce1522378
jakub-guzewicz <56921993+jakub-guzewicz@users.noreply.github.com>
2020-04-28 15:40:26 +0200

    Update SycyReferenceModelTest.java

--- src/SycyReferenceModelTest.java
+++ src/SycyReferenceModelTest.java
@@ -13,21 +13,27 @@ public class SycyReferenceModelTest {
         TEA.encryptTEA(v, realKey);
         Integer[] tmp = {0,0};
         Integer[] key = new Integer[4];
-//        int counter = 0;
+        int counter = 0;
+        System.out.println();
+        long startTime = System.nanoTime();
         while (!tmp[0].equals(desiredValue[0]) || !tmp[1].equals(desiredValue[1])){
             key = keygen.generateKeyLowerCase();
             tmp = TEA.decryptTEA(v,key);
+            counter++;
 //            System.out.println(counter + " " + key[2] + "" + key[3]);
 //            if(counter++ > 1080000000)
 //                throw new Exception("Counter out of range D:");
         }
+        long endTime = System.nanoTime();
+        System.out.println("Time in seconds: "+((endTime-startTime)/1_000_000_000));
+        System.out.println("Number of iterations: "+counter);
         System.out.print("\nKey that we are looking for: ");
         for (Integer i : realKey){
-            System.out.print(i);
+            System.out.print(Integer.toHexString(i)+" ");
         }
         System.out.print("\nKey that we achieved:        ");
         for (Integer i : key){
-            System.out.print(i);
+            System.out.print(Integer.toHexString(i)+" ");
         }
     }
 
